1b9f7742a7372980082e4f271197871069918de3
Toufik Medjber <toufikmedjber2@gmail.com>
2025-05-11 13:29:37 +0200

    Premier commit du projet Unity avec .gitignore

---
+++ Assets/scripts/launcher.cs
@@ -0,0 +1,25 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+}
